9323c060cf04573c3d2d32bb993d0a95434102aa
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-04-25 16:59:00 +0200

    add sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-static';
-// import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
 
 
 /** @type {import('@sveltejs/kit').Config} */
