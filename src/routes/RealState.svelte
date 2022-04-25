196053167dad53d616c24c6eb23abc8bb6a0ce34
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-04-25 20:22:14 +0200

    add realstate page

---
+++ src/routes/RealState.svelte
@@ -0,0 +1,16 @@
+<script>
+import RealStateCard from "../components/RealStateCard.svelte";
+
+</script>
+<main>
+    <h2>real state in rabak </h2>
+</main>
+
+<RealStateCard />
+
+<style lang="scss">
+    main {
+        text-align: center;
+    }
+
+</style>
