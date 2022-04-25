285d1209a182bade6773b144c74ab9967ba1c4f1
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-04-25 20:22:41 +0200

    add real state store

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { writable } from 'svelte/store';
+
+export const RealStates = writable(
+     [{
+        landOwner : "الصادق احمد",
+        location: 349230932904,
+        Image : './alsading-land',
+        discription : "قطعة الارض هذة لها موقع مميز "
+    },{
+        landOwner : "الصادق احمد",
+        location: 349230932904,
+        Image : './alsading-land',
+        discription : "قطعة الارض هذة لها موقع مميز "
+    },{
+        landOwner : "الصادق احمد",
+        location: 349230932904,
+        Image : './alsading-land',
+        discription : "قطعة الارض هذة لها موقع مميز "
+    },{
+        landOwner : "الصادق احمد",
+        location: 349230932904,
+        Image : './alsading-land',
+        discription : "قطعة الارض هذة لها موقع مميز "
+    },{
+        landOwner : "الصادق احمد",
+        location: 349230932904,
+        Image : './alsading-land',
+        discription : "قطعة الارض هذة لها موقع مميز "
+    },]
+);
