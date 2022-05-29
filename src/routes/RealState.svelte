34ec11310429cba2d6c4ce82d91912425ab3cd8a
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-05-29 15:56:09 +0200

    reorginize the realstate component

--- src/routes/RealState.svelte
+++ src/routes/RealState.svelte
@@ -1,15 +1,47 @@
 <script>
-	import RealStateCard from '../components/RealStateCard.svelte';
 </script>
 
-<main>
-	<h2>real state in rabak</h2>
-</main>
+<section>
+	<ul>
+		<li><a href="/RealStates/buy-sell"> بيع وشراء اراضي </a></li>
+		<li><a href="/RealStates/rent"> إيجار </a></li>
+	</ul>
+</section>
 
-<RealStateCard />
-
+<!-- <ul>
+	<li><a href="/goods"> سوق السلع </a></li>
+	<li><a href="/"> المعالم السيا حية </a></li>
+</ul> -->
 <style lang="scss">
-	main {
+	
+	section {
 		text-align: center;
+		font-size: 2rem;
+	}
+	ul {
+		// display: flex;
+		// justify-content: space-around;
+
+		li {
+			list-style-position: inside;
+			list-style-type: none;
+			font-size: 3rem;
+			font-weight: 500;
+			margin: 0.1rem;
+			border-radius: 1rem;
+			background-color: rgb(229, 226, 226);
+			&:hover {
+				color: rgb(219, 197, 218);
+				background-color: rgb(129, 124, 124);
+			}
+			a {
+				text-decoration: none;
+				color: rgb(53, 49, 53);
+				padding: 0.5rem;
+				&:hover {
+					color: rgb(219, 197, 218);
+				}
+			}
+		}
 	}
 </style>
