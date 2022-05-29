38bc2fa107da4e205b01f4df896c4b3931c38d0d
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-05-29 15:52:26 +0200

    a component for all bags

---
+++ src/routes/goods/bags.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Bag from '../../components/bag.svelte';
+	import { bags } from '../../bags-store';
+</script>
+
+<h2>hi there</h2>
+
+<div class="bags">
+	{#each $bags as bag}
+		<Bag description={bag.description} price={bag.price} img={bag.Image} />
+	{/each}
+</div>
+<!-- <div class="bags">
+	<Bag description={$bags[1].description} price={$bags[1].price} img={$bags[1].Image} />
+	<Bag description={$bags[2].description} price={$bags[2].price} img={$bags[2].Image} />
+	<Bag description={$bags[11].description} price={$bags[11].price} img={$bags[11].Image} />
+    <Bag img="../../src/bags-imgs/IMG-20210823-WA0002.jpg" />
+</div> -->
+<style>
+	.bags {
+		display: grid;
+		/* grid-template-columns: 50% 50%; */
+	}
+</style>
