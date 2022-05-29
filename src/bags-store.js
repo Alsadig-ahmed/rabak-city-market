57458a0d16fc352f4fd638aa9a1f8412d9fd04b1
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-05-29 15:15:04 +0200

    add bags store

---
+++ src/bags-store.js
@@ -0,0 +1,64 @@
+import { writable } from 'svelte/store';
+
+export const bags = writable([
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0000.jpg',
+		description: 'شنطة سوداء رقم 1',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0006.jpg',
+		description: 'شنطة سوداء رقم2',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0007.jpg',
+		description: 'شنطة سوداء رقم 3',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0009.jpg',
+		description: 'شنطة زرقاء رقم 4',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0001.jpg',
+		description: 'شنطة زرقاء رقم 1',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0003.jpg',
+		description: 'شنطة زرقاء رقم 2',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0010.jpg',
+		description: 'شنطة زرقاء رقم 3',
+		price:'5$'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0001.jpg',
+		description: 'شنطة رمادية رقم 3',
+		price:'10 SDG'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0004.jpg',
+		description: 'شنطة رمادية رقم 4',
+		price:'10 SDG'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0005.jpg',
+		description: 'شنطة رمادية رقم 1',
+		price:'10 SDG'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0008.jpg',
+		description: 'شنطة رمادية رقم 2',
+		price:'10 SDG'
+	},
+	{
+		Image: '../../src/bags-imgs/IMG-20210823-WA0012.jpg',
+		description: 'شنطة رمادية رقم 4',
+		price:'10 SDG'
+	}
+]);
