2de5317d27523bd5e0c95bff15e9d9e0f5d46dac
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-04 15:58:06 +0200

    add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,14 +9,14 @@ const config = {
 		// ...svelte-preprocess options
 	}),
 	kit: {
-		// adapter: adapter()
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+		adapter: adapter()
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null,
+		// 	precompress: false
+		// }),
 	}
 };
 
