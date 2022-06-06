3bc65eb1bf8e65538daa4f94aae60e941ce8e445
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-06 14:45:24 +0200

    تعديل سعر الحقيبة

--- src/bags-store.js
+++ src/bags-store.js
@@ -4,37 +4,37 @@ export const bags = writable([
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0000.jpg',
 		description: 'شنطة سوداء رقم 1',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0006.jpg',
 		description: 'شنطة سوداء رقم2',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0007.jpg',
 		description: 'شنطة سوداء رقم 3',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0009.jpg',
 		description: 'شنطة زرقاء رقم 4',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0001.jpg',
 		description: 'شنطة زرقاء رقم 1',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0003.jpg',
 		description: 'شنطة زرقاء رقم 2',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0010.jpg',
 		description: 'شنطة زرقاء رقم 3',
-		price:'5$'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0001.jpg',
@@ -44,12 +44,12 @@ export const bags = writable([
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0004.jpg',
 		description: 'شنطة رمادية رقم 4',
-		price:'10 SDG'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0005.jpg',
 		description: 'شنطة رمادية رقم 1',
-		price:'10 SDG'
+		price:'4000'
 	},
 	{
 		Image: '/bags-imgs/IMG-20210823-WA0008.jpg',
