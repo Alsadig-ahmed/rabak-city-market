181695ef1f1378f741afda5aa5cc2b7fb92364e9
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-06 15:09:13 +0200

    تعديل بعض الالوان

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,7 +10,7 @@
 		text-align: center;
 		font-size: 2.4rem;
 		padding: 2rem;
-		background: linear-gradient(to left, rgb(9, 167, 93), rgba(0, 81, 255, 0.856));
+		background: linear-gradient(to left, rgb(141, 22, 135), rgba(100, 123, 173, 0.856));
 		color: rgb(233, 216, 216);
 	}
 </style>
